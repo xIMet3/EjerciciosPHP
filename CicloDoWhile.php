0fd52c09793da8911331295eeda709ac2a63b207
xIMet3 <joaquin19_91@hotmail.com>
2023-11-06 03:56:28 +0100

    Ciclo Do While PHP

---
+++ CicloDoWhile.php
@@ -0,0 +1,17 @@
+<?php
+
+// El ciclo do while primero se ejecuta y luego evalua
+
+$i = 0;
+
+do{
+    // Ejecuta
+    echo $i;
+    $i++;
+
+ // Evalua   
+}while($i <= 10);
+
+
+
+?>
---
+++ CicloDoWhilePractica.php
@@ -0,0 +1,336 @@
+<?php
+
+    //Realiza las tablas de multiplicar que sean del 0 al 20 (Utiliza ciclo Do While)
+    //Tabla del 1
+    $tabla1 = 1;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla1*$i;
+        echo "<br>";
+        echo $tabla1." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 2
+    
+    $tabla2 = 2;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla2*$i;
+        echo "<br>";
+        echo $tabla2." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 3
+
+    $tabla3 = 3;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla3*$i;
+        echo "<br>";
+        echo $tabla3." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 4
+
+    $tabla4 = 4;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla4*$i;
+        echo "<br>";
+        echo $tabla4." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 5
+
+    $tabla5 = 5;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla5*$i;
+        echo "<br>";
+        echo $tabla5." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 6
+
+    $tabla6 = 6;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla6*$i;
+        echo "<br>";
+        echo $tabla6." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 7
+
+    $tabla7 = 7;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla7*$i;
+        echo "<br>";
+        echo $tabla7." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 8
+
+    $tabla8 = 8;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla8*$i;
+        echo "<br>";
+        echo $tabla8." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 9
+
+    $tabla9 = 9;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla9*$i;
+        echo "<br>";
+        echo $tabla9." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 10
+
+    $tabla10 = 10;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla10*$i;
+        echo "<br>";
+        echo $tabla10." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 11
+
+    $tabla11 = 11;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla11*$i;
+        echo "<br>";
+        echo $tabla11." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 12
+
+    $tabla12 = 12;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla12*$i;
+        echo "<br>";
+        echo $tabla12." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 13
+
+    $tabla13 = 13;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla13*$i;
+        echo "<br>";
+        echo $tabla13." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 14
+
+    $tabla14 = 14;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla14*$i;
+        echo "<br>";
+        echo $tabla14." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 15
+
+    $tabla15 = 15;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla15*$i;
+        echo "<br>";
+        echo $tabla15." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 16
+
+    $tabla16 = 16;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla16*$i;
+        echo "<br>";
+        echo $tabla16." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 17
+
+    $tabla17 = 17;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla17*$i;
+        echo "<br>";
+        echo $tabla17." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 18
+
+    $tabla18 = 18;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla18*$i;
+        echo "<br>";
+        echo $tabla18." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 19
+
+    $tabla19 = 19;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla19*$i;
+        echo "<br>";
+        echo $tabla19." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+    echo "<br>";
+
+    //Tabla del 20
+
+    $tabla20 = 20;
+    $i = 0;
+    $resultado;
+
+    do{
+        $resultado = $tabla20*$i;
+        echo "<br>";
+        echo $tabla20." X ".$i." = ".$resultado;
+        $i++;
+
+    }while ($i <= 20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
